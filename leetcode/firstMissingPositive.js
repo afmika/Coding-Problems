aaf5fa002602e171e2bf0f3b94dca8b172eb479f
Michael <afmichael73@gmail.com>
2020-04-14 18:14:30 +0300

    new solutions

--- leetcode/firstMissingPositive.js
+++ leetcode/firstMissingPositive.js
@@ -25,17 +25,25 @@ Your algorithm should run in O(n) time and uses constant extra space.
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    let sol = 1;
 	let max = -1;
     for(let i = 0; i < nums.length; i++) {
-        if(nums[i] == sol )
-            sol++;
 		if(nums[i] > 0) {
 			max = Math.max(max, nums[i]);
 		}
     }
-	if(max == nums[0] && nums.length > 1)
-		sol++;
-    return sol;
+	if(max < 0)
+		return 1;
+	let arr = new Array( max );
+    for(let i = 0; i < nums.length; i++) {
+		if( nums[i] > 0 ) {
+			arr[ nums[i] - 1 ] = nums[i];
+		}
+    }
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] == undefined)
+			return i + 1;
+	}
+	return max + 1;
 };
+
 console.log(firstMissingPositive([1,2,0]));
