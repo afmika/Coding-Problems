d3646ab7fc1c09ffb266c76a817a0216735fe8eb
Michael <afmichael73@gmail.com>
2020-04-27 20:52:01 +0300

    verbal solved slower version

--- leetcode/verbal.js
+++ leetcode/verbal.js
@@ -49,11 +49,88 @@ Constraints:
     Number of different characters used on the expression is at most 10.
 */
 
+
+var areEquals = function (map, words, result) {
+	let df = 0;
+	for(let i = 0; i < words.length; i++) {
+		let no_leading_zero = map[ words[i][0] ] != 0;
+		if ( no_leading_zero ) {
+			for(let c = 0; c < words[i].length; c++) {
+				df += map[ words[i][c] ] * Math.pow(10, words[i].length - c - 1 );
+			}			
+		} else {
+			return false;
+		}
+	}
+	let no_leading_zero = map[ result[0] ] != 0;
+	if ( no_leading_zero ) {
+		for(let c = 0; c < result.length; c++) {
+			df -= map[ result[c] ] * Math.pow(10, result.length - c - 1 );
+		}		
+	} else {
+		return false;
+	}
+	return df == 0;
+}
+
+var clone = function ( obj ) {
+	let res = {};
+	for(let i in obj)
+		res[i] = obj[i];
+	return res;
+}
+
+function combi(map, idx_array, idx, words, result) {
+	console.log( map );
+	if( areEquals(map, words, result) ) {
+		return true;
+	}
+	for(let k = 0; k < 10; k++) {
+		// haha recursion go brrrr
+		let clone_map = clone ( map  );
+		if( idx < idx_array.length) {
+			clone_map[ idx_array[idx] ] = k;
+			if ( combi( clone_map, idx_array, idx + 1, words, result ) ) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 /**
  * @param {string[]} words
  * @param {string} result
  * @return {boolean}
  */
 var isSolvable = function(words, result) {
-    
+    let map = {};
+	for(let i = 0; i < words.length; i++) {
+		for(let c = 0; c < words[i].length; c++) {
+			map[ words[i][c] ] = 0;
+		}
+	}
+	for(let c = 0; c < result.length; c++) {
+		map[ result[c] ] = 0;
+	}
+	let idx_array = [];
+	for(let k in map) {
+		idx_array.push( k );
+	}
+	return combi( map, idx_array, 0, words, result );
 };
+
+console.log( isSolvable(['UN', 'UN'], 'DE') );
+/*
+console.log( areEquals({
+	'S' : 6, 
+	'I' : 5, 
+	'X' : 0, 
+	'E' : 8, 
+	'V' : 7, 
+	'N' : 2, 
+	'T' : 1, 
+	'W' : 3, 
+	'Y' : 4
+}, ["SIX","SEVEN","SEVEN"], "TWENTY") );
+*/
