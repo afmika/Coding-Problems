453fcca397da610fba6d2b015d37b5b111dfb98f
afmika <afmichael73@gmail.com>
2020-11-27 18:16:51 +0300

    Suspects

---
+++ BattleDev/suspects.cpp
@@ -0,0 +1,39 @@
+/*******
+ * Read input from cin
+ * Use cout << ... to output your result to STDOUT.
+ * Use cerr << ... to output debugging information to STDERR
+ * ***/
+#include <iostream>
+#include <limits>
+#include <sstream>
+#include "exercise.hpp"
+
+ContestExerciseImpl::ContestExerciseImpl() : Exercise() {}
+
+void ContestExerciseImpl::main() {
+   std::string line;
+   bool first = true;
+   int suspects = 0;
+   
+	while (std::getline(std::cin, line))
+	{
+	    if ( first ) {
+	        first = false;
+	    }  else {
+	        if ( line.size() >= 5 ) {
+	            int ok = true;
+	            int l = line.size();
+	            for (int i = l - 5; i < (int) l; i++) {
+	                if ( !(line[i] >= '0' && line[i] <= '9') ) {
+	                    ok = false;
+	                    break;
+	                }
+	            }
+    	        if ( ok ) suspects++;
+	        }
+	    }
+		/*Lisez les données ici et effectuez votre traitement ici */
+	}
+    cout << suspects;
+	/* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+}
