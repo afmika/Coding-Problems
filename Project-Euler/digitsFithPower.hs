3a440e879baa641d51b5391198dd6921ec5f3343
afmika <afmichael73@gmail.com>
2021-09-30 15:03:37 +0300

    digitFithPoz

---
+++ Project-Euler/digitsFithPower.hs
@@ -0,0 +1,17 @@
+import Data.Char(digitToInt)
+
+pDigits p n = sum $ map ((^p) . digitToInt) $ show n
+
+isOk p n = (pDigits p n) == n
+
+solve p curr total
+    | curr > 10^(p + 1) = total
+    | otherwise =
+        solve p (curr + 1) ntotal
+        where
+            ntotal = if (isOk p curr) then (total + curr) else total
+
+-- exclude 1 (trivial ans)
+solution n = -1 + solve n 0 0
+
+main = print (solution 5)
